080d72844eb654842fa49038c56fd476dcc22db6
Walaa <walaamousahommadi@gmail.com>
2026-04-19 12:03:44 +0300

    Create CompanyController.php

---
+++ CompanyController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+    
+
+/**class CompanyController extends Controller
+{
+    public function cmp()
+    {
+        $data = ["Apple", "Google"];
+
+        return view('company', compact('data'));
+    }
+}**/
+class CompanyController extends Controller
+{
+    public function camps()
+    {
+        $data = ["Apple", "Google"];
+
+        return view('company', compact('data'));
+    }
+}
